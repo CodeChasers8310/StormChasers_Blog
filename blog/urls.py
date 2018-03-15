61d65d1e2b98897921a09c1f67293ae0cf9c98af
WillieWetz <wcwetzel@unomaha.edu>
2018-03-14 21:03:23 -0500

    adding navbar and login screen

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as authViews
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
-]
+    url(r'^accounts/login/$', authViews.login, name='login'),
+    url(r'^accounts/logout/$', authViews.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^accounts/profile/$', views.post_list, name='post_list'),
+    ]

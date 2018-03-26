25996881b9f20a3c72386bbd914e750e4ef1ea17
klumb <klumbard@unomaha.edu>
2018-03-26 12:48:31 -0500

    email

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/uploadedMedia/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploadedMedia')
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'kmlumbard.dev@gmail.com'
+EMAIL_HOST_PASSWORD = 'larousse6'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
